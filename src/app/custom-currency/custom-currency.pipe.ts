8fcf68d661ea422c0cd2b7d03fda0d9942791d14
Arpit Malaiya <arpitmalaiya@gmail.com>
2022-09-21 10:10:03 +0530

    adding jsdoc

--- src/app/custom-currency/custom-currency.pipe.ts
+++ src/app/custom-currency/custom-currency.pipe.ts
@@ -1,25 +1,57 @@
 import { CurrencyPipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * @description
+ * Formats a value to USD Currrency partially based in parameters.
+ * ForceTwoDecimals determine decimal places if value has any decimals.
+ * Trailing Content determine content appended to valid value.
+ *
+ * @UsageNotes
+ *
+ * ### forceTwoDecimals
+ *
+ * Boolean value to show two decimal places in case any decimal present.
+ *
+ * ### trailingContent
+ *
+ * Following string will be appended to valid currency output value.
+ *
+ * ### Example
+ * <p>{{125.26 | customCurrency : true : 'per year'}}</p>
+ *
+ */
 @Pipe({
   name: 'customCurrency',
 })
 export class CustomCurrencyPipe implements PipeTransform {
   constructor(private currencyPipe: CurrencyPipe) {}
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  /**
+   * @param value The value to be formatted.
+   * @param forceTwoDecimals Sets decimal places to be shown.
+   * [See more](#forceTwoDecimals).
+   * @param trailingContent Content to be appended.
+   * [See more](#trailingContent).
+   */
+  transform(
+    value: number | null | undefined,
+    forceTwoDecimals?: boolean,
+    trailingContent?: string
+  ): string {
     let fCurrency: any = value ?? '-';
-
-    const forceTwoDecimals = args[0];
-
-    const trailingContent:any = args[1] || '';
 
     if (typeof fCurrency === 'number') {
       const format = forceTwoDecimals && fCurrency % 1 > 0 ? '1.2-2' : '1.0-2';
-      fCurrency = this.currencyPipe.transform(fCurrency, 'INR', 'symbol', format);
+      fCurrency = this.currencyPipe.transform(
+        fCurrency,
+        'USD',
+        'symbol',
+        format
+      );
 
       if (trailingContent) {
-        fCurrency = fCurrency.toString().concat(' ', trailingContent.toString());
+        fCurrency = fCurrency.concat(' ', trailingContent);
       }
     }
     return fCurrency;
